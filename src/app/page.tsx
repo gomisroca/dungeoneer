60946901b59f846764650827fbe9c2d5a06fd7f5
asfalfa <glkin9@gmail.com>
2024-10-17 15:01:41 +0200

    fix: placeholder text center

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default async function Home() {
   return (
     <div>
-      <h1 className="text-4xl font-bold text-black dark:text-white">Welcome to Dungeoneer 👋</h1>
+      <h1 className="text-center text-4xl font-bold text-black dark:text-white">Welcome to Dungeoneer 👋</h1>
     </div>
   );
 }
