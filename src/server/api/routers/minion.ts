001ad32e73f2bc99f4e076eb7445be5586077e4a
asfalfa <glkin9@gmail.com>
2024-10-20 14:42:43 +0200

    feat: add minions owned to the session user

--- src/server/api/routers/minion.ts
+++ src/server/api/routers/minion.ts
@@ -1,5 +1,7 @@
 import { z } from 'zod';
-import { createTRPCRouter, publicProcedure } from '@/server/api/trpc';
+import { createTRPCRouter, protectedProcedure, publicProcedure } from '@/server/api/trpc';
+import { TRPCError } from '@trpc/server';
+import { type MinionWithOwners } from 'types';
 
 export const minionRouter = createTRPCRouter({
   getAll: publicProcedure
@@ -12,11 +14,14 @@ export const minionRouter = createTRPCRouter({
     .query(async ({ ctx, input }) => {
       const limit = input.limit ?? 10;
       const { cursor } = input;
-      const minions = await ctx.db.minion.findMany({
+      const minions: MinionWithOwners[] = await ctx.db.minion.findMany({
         take: limit + 1,
         cursor: cursor ? { id: cursor } : undefined,
         orderBy: {
           id: 'desc',
+        },
+        include: {
+          owners: true,
         },
       });
 
@@ -31,5 +36,133 @@ export const minionRouter = createTRPCRouter({
         minions,
         nextCursor,
       };
+    }),
+
+  addToUser: protectedProcedure
+    .input(
+      z.object({
+        minionId: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const minion = await ctx.db.minion.findUnique({
+        where: {
+          id: input.minionId,
+        },
+        include: {
+          owners: true,
+        },
+      });
+
+      if (!minion) {
+        throw new TRPCError({ code: 'NOT_FOUND' });
+      }
+
+      const user = await ctx.db.user.findUnique({
+        where: {
+          id: ctx.session.user.id,
+        },
+        include: {
+          minions: true,
+        },
+      });
+
+      if (!user) {
+        throw new TRPCError({ code: 'NOT_FOUND' });
+      }
+
+      if (user.minions.find((m) => m.id === minion.id)) {
+        throw new TRPCError({ code: 'CONFLICT' });
+      }
+
+      await ctx.db.user.update({
+        where: {
+          id: ctx.session.user.id,
+        },
+        data: {
+          minions: {
+            connect: {
+              id: minion.id,
+            },
+          },
+        },
+      });
+
+      await ctx.db.minion.update({
+        where: {
+          id: minion.id,
+        },
+        data: {
+          owners: {
+            connect: {
+              id: ctx.session.user.id,
+            },
+          },
+        },
+      });
+    }),
+
+  removeFromUser: protectedProcedure
+    .input(
+      z.object({
+        minionId: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const minion = await ctx.db.minion.findUnique({
+        where: {
+          id: input.minionId,
+        },
+        include: {
+          owners: true,
+        },
+      });
+
+      if (!minion) {
+        throw new TRPCError({ code: 'NOT_FOUND' });
+      }
+
+      const user = await ctx.db.user.findUnique({
+        where: {
+          id: ctx.session.user.id,
+        },
+        include: {
+          minions: true,
+        },
+      });
+
+      if (!user) {
+        throw new TRPCError({ code: 'NOT_FOUND' });
+      }
+
+      if (!user.minions.find((m) => m.id === minion.id)) {
+        throw new TRPCError({ code: 'NOT_FOUND' });
+      }
+
+      await ctx.db.user.update({
+        where: {
+          id: ctx.session.user.id,
+        },
+        data: {
+          minions: {
+            disconnect: {
+              id: minion.id,
+            },
+          },
+        },
+      });
+
+      await ctx.db.minion.update({
+        where: {
+          id: minion.id,
+        },
+        data: {
+          owners: {
+            disconnect: {
+              id: ctx.session.user.id,
+            },
+          },
+        },
+      });
     }),
 });
