cbfeb083e86054c2ceeda56ffc5663ce611b3f33
asfalfa <glkin9@gmail.com>
2024-10-20 13:13:42 +0200

    style: adjust button styling

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -7,7 +7,7 @@ function Navbar() {
     <div className="absolute left-0 right-0 top-0 flex flex-row items-center justify-between p-4">
       <Link
         href="/"
-        className="rounded-xl p-3 text-xl font-bold backdrop-blur-sm transition duration-200 ease-in-out hover:bg-cyan-300/80 hover:text-stone-200 dark:hover:bg-cyan-700/80 dark:hover:text-stone-800">
+        className="rounded-xl p-3 text-xl font-bold backdrop-blur-sm transition duration-200 ease-in-out hover:bg-cyan-300 hover:text-stone-900 dark:hover:bg-cyan-700 dark:hover:text-stone-100">
         dungeoneer
       </Link>
       {/* Basic Menu */}
--- src/app/_components/ui/Button.tsx
+++ src/app/_components/ui/Button.tsx
@@ -28,7 +28,7 @@ function Button({ name, type = 'button', disabled = false, onClick, className, c
     name={name || 'Button'}
     type={type} 
     onClick={onClick} 
-    className={twMerge('justify-center whitespace-nowrap hover:text-stone-200 dark:hover:text-stone-800 text-nowrap ease-in-out rounded-xl backdrop-blur-sm dark:hover:bg-cyan-700/80 p-3 font-semibold duration-200 transition hover:bg-cyan-300/80 flex flex-row gap-2 items-center', className, disabled && 'cursor-not-allowed')} disabled={disabled}>
+    className={twMerge('justify-center whitespace-nowrap hover:text-stone-900 dark:hover:text-stone-100 text-nowrap ease-in-out rounded-xl backdrop-blur-sm dark:hover:bg-cyan-700 p-4 font-semibold duration-200 transition hover:bg-cyan-300 flex flex-row gap-2 items-center', className, disabled && 'cursor-not-allowed hover:bg-transparent dark:hover:bg-transparent')} disabled={disabled}>
       {children}
     </button>	
   )
