395d9dee951f12f60949a89ac9a4900f0b52b60d
asfalfa <glkin9@gmail.com>
2024-10-21 22:44:22 +0200

    style: add feedback on button click

--- src/app/_components/navbar/NavbarWrapper.tsx
+++ src/app/_components/navbar/NavbarWrapper.tsx
@@ -11,7 +11,7 @@ async function Navbar() {
     <div className="absolute left-0 right-0 top-0 flex flex-row items-start justify-between p-4">
       <Link
         href="/"
-        className="rounded-xl p-3 text-xl font-bold backdrop-blur-sm transition duration-200 ease-in-out hover:bg-cyan-300 hover:text-stone-900 dark:hover:bg-cyan-700 dark:hover:text-stone-100">
+        className="rounded-xl p-3 text-xl font-bold backdrop-blur-sm transition duration-200 ease-in-out hover:bg-cyan-300 hover:text-stone-900 active:scale-x-110 active:bg-cyan-300 active:duration-100 dark:hover:bg-cyan-700 dark:hover:text-stone-100 dark:active:bg-cyan-700">
         dungeoneer
       </Link>
       {/* Basic Menu */}
