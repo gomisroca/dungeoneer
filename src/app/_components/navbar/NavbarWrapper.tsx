c38781e83d4e574accb5c3b47eb0e4667bda6ccb
asfalfa <glkin9@gmail.com>
2024-10-22 10:22:28 +0200

    style: make navbar fixed

--- src/app/_components/navbar/NavbarWrapper.tsx
+++ src/app/_components/navbar/NavbarWrapper.tsx
@@ -8,7 +8,7 @@ import ThemeButton from './themeButton';
 async function Navbar() {
   const session = await getServerAuthSession();
   return (
-    <div className="absolute left-0 right-0 top-0 flex flex-row items-start justify-between p-4">
+    <div className="fixed left-0 right-0 top-0 flex flex-row items-start justify-between p-4">
       <Link
         href="/"
         className="rounded-xl p-3 text-xl font-bold backdrop-blur-sm transition duration-200 ease-in-out hover:bg-cyan-300 hover:text-stone-900 active:scale-x-110 active:bg-cyan-300 active:duration-100 dark:hover:bg-cyan-700 dark:hover:text-stone-100 dark:active:bg-cyan-700">
