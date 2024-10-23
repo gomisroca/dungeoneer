51009a39eac55f41374bfe0ef37c625668729d9f
asfalfa <glkin9@gmail.com>
2024-10-23 12:59:55 +0200

    feat: add expanded types

--- types.ts
+++ types.ts
@@ -1,10 +1,35 @@
-import { type Minion, type User, type Dungeon, type MinionSource } from '@prisma/client';
+import {
+  type Minion,
+  type User,
+  type Dungeon,
+  type MinionSource,
+  type Raid,
+  type Trial,
+  type Mount,
+  type MountSource,
+} from '@prisma/client';
 
 export interface ExpandedMinion extends Minion {
   owners: User[];
   sources: MinionSource[];
 }
 
+export interface ExpandedMount extends Mount {
+  owners: User[];
+  sources: MountSource[];
+}
+
 export interface ExpandedDungeon extends Dungeon {
   minions: ExpandedMinion[];
+  mounts: ExpandedMount[];
+}
+
+export interface ExpandedRaid extends Raid {
+  minions: ExpandedMinion[];
+  mounts: ExpandedMount[];
+}
+
+export interface ExpandedTrial extends Trial {
+  minions: ExpandedMinion[];
+  mounts: ExpandedMount[];
 }
