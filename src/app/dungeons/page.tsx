716172e119604fff0c7f964ed154f739696137de
asfalfa <glkin9@gmail.com>
2024-10-27 10:39:28 +0100

    fix: 504 #2

--- src/app/dungeons/page.tsx
+++ src/app/dungeons/page.tsx
@@ -1,9 +1,17 @@
 import { getServerAuthSession } from '@/server/auth';
-import DungeonList from './DungeonList';
 import { api } from '@/trpc/server';
+import { Suspense } from 'react';
+import Loading from './loading';
+import dynamic from 'next/dynamic';
+
+const DungeonList = dynamic(() => import('./DungeonList'));
 
 export default async function DungeonListWrapper() {
   const initialDungeons = await api.dungeons.getAll({ limit: 9 });
   const session = await getServerAuthSession();
-  return <DungeonList session={session} initialDungeons={initialDungeons} />;
+  return (
+    <Suspense fallback={<Loading />}>
+      <DungeonList session={session} initialDungeons={initialDungeons} />
+    </Suspense>
+  );
 }
