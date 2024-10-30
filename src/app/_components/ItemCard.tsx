6f5fcd13aa3cbafa07de041ff42578430677a5ca
asfalfa <glkin9@gmail.com>
2024-10-30 14:30:48 +0100

    style: adjust positioning of add/remove button

--- src/app/_components/ItemCard.tsx
+++ src/app/_components/ItemCard.tsx
@@ -44,7 +44,7 @@ export default function ItemCard({ item, type, session }: ItemCardProps) {
   return (
     <div
       className={twMerge(
-        'relative flex h-full flex-col items-center justify-start gap-y-4 rounded-xl border-4 border-zinc-200 bg-zinc-300 p-4 font-semibold shadow-md transition duration-200 ease-in hover:z-[99] hover:rotate-2 hover:scale-125 hover:shadow-2xl dark:border-zinc-800 dark:bg-zinc-700',
+        'relative flex h-full flex-col items-center justify-between gap-y-4 rounded-xl border-4 border-zinc-200 bg-zinc-300 p-4 font-semibold shadow-md transition duration-200 ease-in hover:z-[99] hover:rotate-2 hover:scale-125 hover:shadow-2xl dark:border-zinc-800 dark:bg-zinc-700',
         owned && 'opacity-50 hover:opacity-100'
       )}>
       {owned && (
@@ -54,20 +54,22 @@ export default function ItemCard({ item, type, session }: ItemCardProps) {
           </span>
         </div>
       )}
-      {item.image && (
-        <Image
-          unoptimized
-          src={item.image}
-          alt={item.name}
-          width={100}
-          height={100}
-          className={twMerge(
-            'rounded-xl',
-            (type === 'spells' || type === 'emotes') && 'border-2 border-zinc-400 dark:border-zinc-600'
-          )}
-        />
-      )}
-      <h1 className="line-clamp-2 text-center text-lg">{item.name}</h1>
+      <div className="flex flex-col items-center justify-center gap-y-2">
+        {item.image && (
+          <Image
+            unoptimized
+            src={item.image}
+            alt={item.name}
+            width={100}
+            height={100}
+            className={twMerge(
+              'rounded-xl',
+              (type === 'spells' || type === 'emotes') && 'border-2 border-zinc-400 dark:border-zinc-600'
+            )}
+          />
+        )}
+        <h1 className="line-clamp-2 text-center text-lg">{item.name}</h1>
+      </div>
       {item.sources && (
         <div className="flex flex-wrap items-center justify-center gap-2 md:gap-4 md:p-4">
           {item.sources.map((source) => (
