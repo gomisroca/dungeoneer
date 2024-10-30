d241197fa2f04f8b43d9c7638712a57400eedcc2
asfalfa <glkin9@gmail.com>
2024-10-30 13:59:09 +0100

    feat: hide sync button if localStorage items empty

--- src/app/_components/navbar/SyncButton.tsx
+++ src/app/_components/navbar/SyncButton.tsx
@@ -4,13 +4,25 @@ import useItemSync from '@/hooks/useItemSync';
 import Button from '../ui/Button';
 import { type Session } from 'next-auth';
 import { MdOutlineSync } from 'react-icons/md';
+import { useEffect, useState } from 'react';
 
 export default function SyncButton({ session }: { session: Session | null }) {
-  const { syncCollection, isSyncing } = useItemSync({ session });
+  const [syncCount, setSyncCount] = useState(0);
+  const { syncCollection, isSyncing, getLocalItems } = useItemSync({ session });
+
+  useEffect(() => {
+    const localItems = getLocalItems();
+    if (localItems.length > 0) {
+      setSyncCount(localItems.length);
+    }
+  }, [getLocalItems, syncCollection]);
 
   return (
-    <Button onClick={syncCollection} disabled={isSyncing}>
-      <MdOutlineSync size={20} className={`${isSyncing ? 'animate-spin' : ''}`} />
+    <Button
+      onClick={syncCollection}
+      disabled={isSyncing || syncCount === 0}
+      className={`${syncCount === 0 && 'hidden'}`}>
+      <MdOutlineSync size={20} className={`${isSyncing && 'animate-spin'}`} />
     </Button>
   );
 }
