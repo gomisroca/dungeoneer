72c7b843d91b4b95034fb7407f88dbb5ad6950d0
asfalfa <glkin9@gmail.com>
2025-02-06 14:49:48 +0100

    feat: get ex searchparam and pass it to queries

--- src/app/collectible/[type]/page.tsx
+++ src/app/collectible/[type]/page.tsx
@@ -4,16 +4,27 @@ import { Suspense } from 'react';
 import Loading from './loading';
 import dynamic from 'next/dynamic';
 import { notFound } from 'next/navigation';
-import { COLLECTIBLE_TYPES } from '@/utils/consts';
+import { COLLECTIBLE_TYPES, EXPANSIONS } from '@/utils/consts';
 
 const CollectibleList = dynamic(() => import('./CollectibleList'));
 
-export default async function CollectibleListWrapper({ params }: { params: Promise<{ type: string }> }) {
+export default async function CollectibleListWrapper({
+  params,
+  searchParams,
+}: {
+  params: Promise<{ type: string }>;
+  searchParams?: Record<string, string | string[] | undefined>;
+}) {
   const type = (await params).type;
   if (!type || !COLLECTIBLE_TYPES.includes(type)) return notFound();
-
   const routeKey = type as 'cards' | 'minions' | 'mounts' | 'spells' | 'orchestrions' | 'emotes' | 'hairstyles';
-  const initialCollectibles = await api[routeKey].getAll({ limit: 30 });
+
+  const expansion = searchParams?.ex ?? undefined;
+
+  const initialCollectibles = await api[routeKey].getAll({
+    limit: 30,
+    expansion: EXPANSIONS[expansion as keyof typeof EXPANSIONS],
+  });
   const session = await getServerAuthSession();
 
   return (
--- src/app/instance/[type]/page.tsx
+++ src/app/instance/[type]/page.tsx
@@ -3,17 +3,28 @@ import { api } from '@/trpc/server';
 import { Suspense } from 'react';
 import Loading from './loading';
 import dynamic from 'next/dynamic';
-import { INSTANCE_TYPES } from '@/utils/consts';
+import { EXPANSIONS, INSTANCE_TYPES } from '@/utils/consts';
 import { notFound } from 'next/navigation';
 
 const InstanceList = dynamic(() => import('./InstanceList'));
 
-export default async function InstanceListWrapper({ params }: { params: Promise<{ type: string }> }) {
+export default async function InstanceListWrapper({
+  params,
+  searchParams,
+}: {
+  params: Promise<{ type: string }>;
+  searchParams?: Record<string, string | string[] | undefined>;
+}) {
   const type = (await params).type;
   if (!type || !INSTANCE_TYPES.includes(type)) return notFound();
-
   const routeKey = type as 'dungeons' | 'raids' | 'trials' | 'variants';
-  const initialInstances = await api[routeKey].getAll({ limit: 18 });
+
+  const expansion = searchParams?.ex ?? undefined;
+
+  const initialInstances = await api[routeKey].getAll({
+    limit: 20,
+    expansion: EXPANSIONS[expansion as keyof typeof EXPANSIONS],
+  });
   const session = await getServerAuthSession();
   return (
     <Suspense fallback={<Loading />}>
