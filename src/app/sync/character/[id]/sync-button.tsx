73a96d39da4731384b049635b8af2436ac7d33bf
Adrià Gomis <gomisroca@gmail.com>
2025-06-09 15:40:01 +0200

    feat: replace tRPC mutation with server action

---
+++ src/app/sync/character/[id]/sync-button.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { syncLodestone } from '@/actions/lodestone';
+import Button from '@/app/_components/ui/Button';
+import { useMessage } from '@/hooks/useMessage';
+import { toErrorMessage } from '@/utils/errors';
+import { type Session } from 'next-auth';
+
+function SyncButton({ session, lodestoneId }: { session: Session | null; lodestoneId: string }) {
+  const setMessage = useMessage();
+
+  const handleSync = async () => {
+    try {
+      // Get confirmation from the user
+      const confirmed = confirm(
+        'Are you sure you want to sync your character?\nThis will update your minion and mount collections to match those of the character on Lodestone.\nYour current collection will be replaced.'
+      );
+      if (!confirmed) return;
+
+      // Optimistically set the 'in progress' message
+      setMessage({ content: 'Syncing character...' });
+
+      // Call the server action
+      await syncLodestone({ lodestoneId });
+
+      // Once the server action is completed, set the 'success' message
+      setMessage({ content: 'Character synced successfully.' });
+    } catch (error) {
+      // If there was an error during the action, set the error message
+      setMessage({ content: toErrorMessage(error, 'Syncing character failed.'), error: true });
+    }
+  };
+
+  return (
+    <section className="flex flex-col items-center justify-center gap-2">
+      <Button className="mx-auto w-64" disabled={!session} onClick={handleSync}>
+        Sync Character
+      </Button>
+      {!session && <p className="text-sm">You must be logged in to sync your character.</p>}
+    </section>
+  );
+}
+
+export default SyncButton;
