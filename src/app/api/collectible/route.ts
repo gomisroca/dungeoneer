57848510b36089cdc4a4223bf7a4bf3f841ca4a9
Adrià Gomis <gomisroca@gmail.com>
2025-06-11 14:22:51 +0200

    fix: nullish coalescing operator

--- src/app/api/collectible/route.ts
+++ src/app/api/collectible/route.ts
@@ -1,14 +1,15 @@
+import { type ItemRouteKey } from 'types';
+
 import { fetchItems } from '@/server/queries/items';
 import { EXPANSIONS } from '@/utils/consts';
 import { itemKeytoModel } from '@/utils/mappers';
-import { ItemRouteKey } from 'types';
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
   const type = searchParams.get('type')! as ItemRouteKey;
   const expansion = searchParams.get('expansion')!;
-  const skip = parseInt(searchParams.get('skip') || '0', 10);
-  const take = parseInt(searchParams.get('take') || '20', 10);
+  const skip = parseInt(searchParams.get('skip') ?? '0', 10);
+  const take = parseInt(searchParams.get('take') ?? '20', 10);
 
   if (!type || !itemKeytoModel[type]) {
     return new Response('Invalid type', { status: 400 });
--- src/app/api/instance/route.ts
+++ src/app/api/instance/route.ts
@@ -1,14 +1,15 @@
+import { type InstanceRouteKey } from 'types';
+
 import { fetchInstances } from '@/server/queries/instances';
 import { EXPANSIONS } from '@/utils/consts';
 import { instanceKeytoModel } from '@/utils/mappers';
-import { InstanceRouteKey } from 'types';
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
   const type = searchParams.get('type')! as InstanceRouteKey;
   const expansion = searchParams.get('expansion')!;
-  const skip = parseInt(searchParams.get('skip') || '0', 10);
-  const take = parseInt(searchParams.get('take') || '20', 10);
+  const skip = parseInt(searchParams.get('skip') ?? '0', 10);
+  const take = parseInt(searchParams.get('take') ?? '20', 10);
 
   if (!type || !instanceKeytoModel[type]) {
     return new Response('Invalid type', { status: 400 });
