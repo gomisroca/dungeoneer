2469f666bf74403b9da8178cf10a9e5a219e1b07
Adrià Gomis <gomisroca@gmail.com>
2025-06-12 09:16:55 +0200

    fix: auth api route

--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -7,4 +7,4 @@ const { auth: uncachedAuth, handlers, signIn, signOut } = NextAuth(authConfig);
 
 const auth = cache(uncachedAuth);
 
-export { auth, authConfig, handlers, signIn, signOut };
+export { auth, handlers, signIn, signOut };
