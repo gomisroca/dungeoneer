a8f8f66321b0a734f357fa26248efd2abb4ccfed
Adrià Gomis <gomisroca@gmail.com>
2025-06-12 09:17:51 +0200

    style: add slight interactivity hints

--- src/app/_components/ui/source.tsx
+++ src/app/_components/ui/source.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './tooltip'
 import { useEffect, useRef, useState } from 'react';
 import { useAutoAnimate } from '@formkit/auto-animate/react'
+import { twMerge } from 'tailwind-merge';
 
 function Source({ source, compact = false }: { source: MinionSource | MountSource | OrchestrionSource | SpellSource | EmoteSource | HairstyleSource | CardSource; compact?: boolean }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -41,7 +42,7 @@ function Source({ source, compact = false }: { source: MinionSource | MountSourc
               alt={source.type}
               width={compact ? 30 : 50}
               height={compact ? 30 : 50}
-              className='active:scale-110 active:contrast-125 active:duration-100 hover:scale-110 transition duration-200 ease-in-out hover:contrast-125 h-12 object-contain'
+              className={twMerge('active:scale-110 active:contrast-125 rounded-lg border-2 border-zinc-400 dark:border-zinc-600 dark:bg-zinc-800/90 bg-zinc-200/90 hover:border-cyan-300 dark:hover:border-cyan-700 active:duration-100 hover:scale-110 transition duration-200 ease-in-out hover:contrast-125 object-contain', compact ? 'h-9 w-9' : 'h-12 w-12')}
             />
           </TooltipTrigger>
             {isOpen && (
