1296e94144dc2aaedf09cff371020481b5f5e49d
Adrià Gomis <gomisroca@gmail.com>
2025-06-12 10:12:00 +0200

    style: adjust bg color of non-filled icons

--- src/app/_components/ui/source.tsx
+++ src/app/_components/ui/source.tsx
@@ -42,7 +42,7 @@ function Source({ source, compact = false }: { source: MinionSource | MountSourc
               alt={source.type}
               width={compact ? 30 : 50}
               height={compact ? 30 : 50}
-              className={twMerge('active:scale-110 active:contrast-125 rounded-lg border-2 border-zinc-400 dark:border-zinc-600 dark:bg-zinc-800/90 bg-zinc-200/90 hover:border-cyan-300 dark:hover:border-cyan-700 active:duration-100 hover:scale-110 transition duration-200 ease-in-out hover:contrast-125 object-contain', compact ? 'h-9 w-9' : 'h-12 w-12')}
+              className={twMerge('active:scale-110 active:contrast-125 rounded-lg border-2 border-zinc-400 dark:border-zinc-600 dark:bg-zinc-600 bg-zinc-400 hover:border-cyan-300 dark:hover:border-cyan-700 active:duration-100 hover:scale-110 transition duration-200 ease-in-out hover:contrast-125 object-contain', compact ? 'h-9 w-9' : 'h-12 w-12')}
             />
           </TooltipTrigger>
             {isOpen && (
