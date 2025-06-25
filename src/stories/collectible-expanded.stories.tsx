e94245f91f7e50f564dc0e601b07476172b82434
Adrià Gomis <gomisroca@gmail.com>
2025-06-25 12:50:21 +0200

    feat: add collectible related stories

---
+++ src/stories/collectible-expanded.stories.tsx
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import { type ExpandedMinion } from 'types';
+
+import { ItemCard } from '@/app/_components/ui/cards';
+
+const meta = {
+  title: 'Cards/Collectible/Expanded',
+  component: ItemCard,
+
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: {},
+} satisfies Meta<typeof ItemCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const minion: ExpandedMinion = {
+  id: '1',
+  name: 'Minion 1',
+  image: '/xivlogo.png',
+  owners: [{ id: '1', name: 'Owner 1', image: '', email: '', emailVerified: new Date() }],
+  sources: [{ id: '1', type: 'Dungeon', text: 'Halatali', minionId: '1' }],
+  description: 'This is a basic minion description.',
+  shortDescription: 'A cute minion.',
+  tradeable: true,
+  behavior: 'Passive',
+  race: 'Bug',
+  patch: '1.0',
+  owned: '10%',
+};
+
+export const Basic: Story = {
+  args: {
+    item: minion,
+    type: 'minions',
+    session: null,
+  },
+};
