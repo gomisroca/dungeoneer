e0948050f7b4ea8bd8e2590471250337f8449c3b
Adrià Gomis <gomisroca@gmail.com>
2025-06-25 13:21:43 +0200

    fix: more robust toggle logic

--- src/app/_components/view-toggler.tsx
+++ src/app/_components/view-toggler.tsx
@@ -6,13 +6,13 @@ import Button from '@/app/_components/ui/button';
 function ViewToggler({ onViewChange }: { onViewChange: (view: boolean) => void }) {
   const [view, setView] = useState(false);
 
-  function handleViewChange(view: boolean) {
-    onViewChange(view);
-    setView(view);
+  function handleViewChange() {
+    setView((prev) => !prev);
+    onViewChange(!view);
   }
   return (
     <div className="fixed top-1 right-[5.5rem] z-20 flex flex-col items-end justify-end gap-2 p-4 md:right-[8.5rem]">
-      <Button onClick={() => handleViewChange(!view)} className="h-[32px] w-[32px] p-2 md:h-full md:w-fit md:p-3">
+      <Button onClick={() => handleViewChange()} className="h-[32px] w-[32px] p-2 md:h-full md:w-fit md:p-3">
         <span className="sr-only">{view ? 'Compact View' : 'Compact View Off'}</span>
         {view ? <LuShrink size={20} /> : <LuExpand size={20} />}
       </Button>
