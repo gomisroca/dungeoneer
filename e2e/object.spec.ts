c1590a7c550e76a2eef94282cafd809685456c7d
Adrià Gomis <gomisroca@gmail.com>
2025-06-27 15:57:33 +0200

    chore: adjust import order

--- e2e/object.spec.ts
+++ e2e/object.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test('has object cards', async ({ page }) => {
   await page.goto('/collectible/minions');
