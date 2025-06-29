b7a7fb10f2b01b0cc5c6db8713f36f8628c138a8
Adrià Gomis <gomisroca@gmail.com>
2025-06-29 10:40:35 +0200

    feat: add additional e2e test to instance and collectible views

--- e2e/instance.spec.ts
+++ e2e/instance.spec.ts
@@ -22,3 +22,48 @@ test('object content is clickable', async ({ page }) => {
   await expect(page.getByText('✔').first()).toBeVisible();
   await expect(page.getByText('✔A Thousand Screams')).toBeVisible();
 });
+
+test('different instance types have different object content', async ({ page }) => {
+  await page.goto('/instance/trials');
+
+  await expect(
+    page
+      .locator('div')
+      .filter({ hasText: /^The Bowl of Embers Hard$/ })
+      .getByRole('heading')
+  ).toBeVisible();
+  await expect(
+    page
+      .locator('div')
+      .filter({ hasText: /^The Navel Hard$/ })
+      .getByRole('heading')
+  ).toBeVisible();
+});
+
+test('size of cards can be toggled', async ({ page }) => {
+  await page.goto('/instance/dungeons');
+
+  const sizeButton = page.locator('button').filter({ hasText: 'Compact View Off' });
+
+  await expect(page.getByRole('img', { name: 'the Tam–Tara Deepcroft' })).not.toBeVisible();
+  await expect(sizeButton).toBeVisible();
+
+  await sizeButton.click();
+
+  await expect(page.getByRole('img', { name: 'the Tam–Tara Deepcroft' })).toBeVisible();
+});
+
+test('can be filtered by expansion', async ({ page }) => {
+  await page.goto('/instance/dungeons');
+
+  const expansionButton = page.locator('button').filter({ hasText: 'Filter Menu' });
+  await expect(expansionButton).toBeVisible();
+
+  await expansionButton.click();
+
+  const dtButton = page.locator('button').filter({ hasText: 'Dawntrail' });
+  await expect(dtButton).toBeVisible();
+
+  await dtButton.click();
+  await expect(page).toHaveURL(/ex=dt/);
+});
