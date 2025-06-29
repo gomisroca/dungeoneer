0d3e8aa50e409df99dbe40becb38d08cc3d62b86
Adrià Gomis <gomisroca@gmail.com>
2025-06-29 11:19:29 +0200

    fix: link bg color fits button styling

--- src/app/_components/ui/link.tsx
+++ src/app/_components/ui/link.tsx
@@ -6,7 +6,7 @@ function Link({ children, href, className }: { children: React.ReactNode, href: 
   return (
     <NextLink
     href={href}
-    className={twMerge("flex flex-row items-center shadow-md hover:drop-shadow-lg dark:bg-zinc-800/50 bg-zinc-100/25 justify-center gap-2 whitespace-nowrap text-nowrap rounded-md p-4 font-semibold transition duration-200 ease-in-out hover:bg-cyan-300 hover:text-zinc-900 active:scale-x-110 active:bg-cyan-300 active:duration-100 dark:hover:bg-cyan-700 dark:hover:text-zinc-100 dark:active:bg-cyan-700", className)}>
+    className={twMerge("flex flex-row items-center shadow-md hover:drop-shadow-lg dark:bg-zinc-800/50 bg-zinc-200/50 justify-center gap-2 whitespace-nowrap text-nowrap rounded-md p-4 font-semibold transition duration-200 ease-in-out hover:bg-cyan-300 hover:text-zinc-900 active:scale-x-110 active:bg-cyan-300 active:duration-100 dark:hover:bg-cyan-700 dark:hover:text-zinc-100 dark:active:bg-cyan-700", className)}>
       {children}
     </NextLink>
   )
